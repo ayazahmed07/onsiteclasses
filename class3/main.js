0b518d30a8d8c18f804ca2a5fde6bbbc6fb7cbec
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-17 16:03:51 +0500

    Add files via upload

---
+++ class3/main.js
@@ -0,0 +1,35 @@
+// Turnary Operator
+let age = 19;
+let result = age >= 18 ? "you are allowed" : "you are not allowed";
+console.log(result);
+export {};
+// let myPercentage = 90;
+// if (myPercentage >= 90) {
+//   console.log("Your Grade is A+");
+// } else if (myPercentage > 70) {
+//   console.log("Your Grade is A");
+// } else if (myPercentage > 50) {
+//   console.log("Your Grade is B");
+// } else if (myPercentage > 30) {
+//   console.log("Your Grade is C");
+// } else {
+//   console.log("Failed");
+// }
+// let iskhubsoorat = false;
+// let isAmeer = false;
+// if (iskhubsoorat || isAmeer) {
+//   console.log("Rishta Pakka");
+// } else {
+//   console.log("Rejected");
+// }
+// && Operator in if & else condition
+// let achijob = false;
+// let cancook = false;
+// let salary = 90000;
+// if(achijob && cancook && salary >= 100000) {console.log("Rishta Pakka!!!")}
+// else {console.log("rejected");}
+// if else conditions
+// let percentage = 88;
+// if (percentage <= 87) {
+//     console.log("Hello do a party");
+// } else {console.log("Not Sure about party");}
