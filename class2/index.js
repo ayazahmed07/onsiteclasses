50a4abdb52c65c73ecd17e3c98f2b0488f15f61f
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-17 16:02:55 +0500

    Add files via upload

---
+++ class2/index.js
@@ -0,0 +1,54 @@
+"use strict";
+// let numberofFaculty: number = 4;
+// let numberofManagement: number = 3;
+// let numberofabsentfaculty: number = 2;
+// let numberOfbatches: number = 21
+// let totalNumberofFaculty = numberofFaculty * numberOfbatches;
+// let biryani = 20
+// let biryaniPerhead = biryani / totalNumberofFaculty;
+// console.log(`Total Number of Faculty = ${totalNumberofFaculty}`);
+// console.log(`Per person biryani required = ${biryaniPerhead + " " + "KG"}`);
+// let firstname = "Ayaz";
+// let age = 29;
+// // console.log("My name is: " + firstname + "and my age is:" + age);
+// console.log(`My name is: ${firstname} and my age is: ${age}`) 
+// Urinary Operator
+// let index = 10;
+// ++index;
+// ++index;
+// --index;
+// --index;
+// --index;
+// --index;
+// console.log(index);
+// let index1 = 10;
+// --index;
+// --index;
+// --index;
+// --index;
+// --index;
+// --index;
+// console.log(index1);
+// let index = 10;
+// let result = ++index + ++index + 6 + --index - 2;
+// console.log(result);
+let a = 5;
+let b = 2;
+let c;
+//   6 +  6 + 1 + 1 + 7 + 1 + 1 = 
+c = ++a + a++ + --b + b-- + a + b;
+console.log(c);
+// Assignment Operator = 
+// let num1:number = 20;
+// num1 += 2;
+// console.log(num1);
+// let num2:number = 20;
+// num2 -= 22;
+// console.log(num2);
+// Comparison Operator ==
+// let num1 = 4
+// let num2 = 6
+// console.log(num1 == num2);
+// let num1 = 4
+// let num2 = 4
+// console.log(num1 == num2);
