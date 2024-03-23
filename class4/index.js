e0fa655c571132535ab40dbef4bb97a4cfc0e58b
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-23 23:14:18 +0500

    updated index.ts

--- class4/index.js
+++ class4/index.js
@@ -1,1 +1,2 @@
-console.log("Hello Ayaz");
+var keyword = Math.floor(Math.random() * 8 + 1);
+console.log(keyword);
--- class4/index.ts
+++ class4/index.ts
@@ -1,1 +1,2 @@
-console.log("Hello Ayaz");
+const keyword = Math.floor(Math.random() * 8 +1 );
+console.log(keyword);
