2ce19268d0e9b73c040362c46498cbadc6bb6a1b
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-30 12:24:57 +0500

    v2

--- class5/main.ts
+++ class5/main.ts
@@ -16,12 +16,8 @@ function sum(num1: number, num2: number) {
   // after sub in ()we will type parameters
   let answer = num1 + num2;
   return answer; // this means function is end here
-
-
 }
 
 let result = sum(5, 2);
 
 console.log(result);
-
-
