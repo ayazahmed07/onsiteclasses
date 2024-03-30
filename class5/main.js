e6198a64ede6aa373ab7f1cdc28eb73d19e51cb4
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-30 12:23:48 +0500

    updated v1

--- class5/main.js
+++ class5/main.js
@@ -1,6 +1,18 @@
-"use strict";
-function greet() {
-    return "Hello World Printed by Ayaz";
+// function greet (){
+//     return "Hello World Printed by Ayaz";
+// }
+// let message = greet();
+// console.log(message);
+// function sum() {
+//   return 2 + 2;
+// }
+// let result = sum();
+// console.log(result);
+function sum(num1, num2) {
+    // after sub in ()we will type parameters
+    let answer = num1 + num2;
+    return answer; // this means function is end here
 }
-let message = greet();
-console.log(message);
+let result = sum(5, 2);
+console.log(result);
+export {};
--- class5/main.ts
+++ class5/main.ts
@@ -1,7 +1,27 @@
-function greet (){
-    return "Hello World Printed by Ayaz";
+// function greet (){
+//     return "Hello World Printed by Ayaz";
+// }
+// let message = greet();
+// console.log(message);
+
+// function sum() {
+//   return 2 + 2;
+// }
+
+// let result = sum();
+
+// console.log(result);
+
+function sum(num1: number, num2: number) {
+  // after sub in ()we will type parameters
+  let answer = num1 + num2;
+  return answer; // this means function is end here
+
+
 }
-let message = greet();
-console.log(message);
+
+let result = sum(5, 2);
+
+console.log(result);
 
 
