d735af014e3ba284d360aafbe330cac763beedb1
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-04-20 15:39:11 +0500

    class work

--- class6/main.js
+++ class6/main.js
@@ -1,6 +1,25 @@
 "use strict";
+// let age = 30; // global variable can be used every where
 Object.defineProperty(exports, "__esModule", { value: true });
-if (true) {
-    let age = 30;
-}
-console.log(age);
+// if (true) {
+//   console.log(age);
+// } // curly bracket is called block - varaible which is inside a currly bracked is called a local variable which
+// // can not be used outside the block
+// // Hoisting
+// let age1; // declares a variable
+// age1 = 24; // initailize a variable
+// console.log(age1);
+// var age2 = 90;
+// console.log(age2);
+// // wherever we use varaible var it will go on top because of hoisting even if we declare it after console.
+// Basic Objects in Typescript
+let car = {
+    name: "Toyota",
+    Model: 2022,
+    Make: "Carolla",
+    Chasis_No: 103412366788,
+};
+console.log(car);
+console.log(car.Model);
+console.log(car.Make);
+console.log(car.Chasis_No);
--- class6/main.ts
+++ class6/main.ts
@@ -1,5 +1,36 @@
-let age = 30; // global variable can be used every where 
+// let age = 30; // global variable can be used every where
 
-if (true) {
-  console.log(age);
-} // curly bracket is called block
+// if (true) {
+//   console.log(age);
+// } // curly bracket is called block - varaible which is inside a currly bracked is called a local variable which
+// // can not be used outside the block
+
+// // Hoisting
+
+// let age1; // declares a variable
+
+// age1 = 24; // initailize a variable
+
+// console.log(age1);
+
+// var age2 = 90;
+
+// console.log(age2);
+
+// // wherever we use varaible var it will go on top because of hoisting even if we declare it after console.
+
+
+
+// Basic Objects in Typescript
+
+
+let car = {
+name: "Toyota",
+Model: 2022,
+Make: "Carolla",
+Chasis_No : 103412366788,
+
+
+}
+console.log(car);
+
