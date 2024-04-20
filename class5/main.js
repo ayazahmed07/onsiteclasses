9698f1eb7c8f3ee4c8acfd610a5e894ea7207d41
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-04-20 14:27:35 +0500

    updated

--- class5/main.js
+++ class5/main.js
@@ -8,11 +8,25 @@
 // }
 // let result = sum();
 // console.log(result);
-function sum(num1, num2) {
-    // after sub in ()we will type parameters
-    let answer = num1 + num2;
-    return answer; // this means function is end here
-}
-let result = sum(5, 2);
-console.log(result);
+// function sum(num1: number, num2: number) {
+//   // after sub in ()we will type parameters
+//   let answer = num1 + num2;
+//   return answer; // this means function is end here
+// }
+// let result = sum(5, 2);
+// console.log(result);
+// function sum(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// let result = sum(10, 2);
+// console.log(result);
+// function greet(message = "Hellow Ayaz") {
+//   //this is called parameter
+//   return message;
+// }
+// let result = greet("Ayaz ahmed");
+// console.log(result);
+// Arrow Function
+let greet = () => "Hellow World";
+console.log(greet());
 export {};
