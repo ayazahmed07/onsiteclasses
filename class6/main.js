0e7468923975a7a4f8b781a4168a0a8f1407dda1
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-04-20 14:33:38 +0500

    main.ts

---
+++ class6/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+if (true) {
+    let age = 30;
+}
+console.log(age);
---
+++ class6/main.ts
@@ -0,0 +1,5 @@
+let age = 30; // global variable can be used every where 
+
+if (true) {
+  console.log(age);
+} // curly bracket is called block
