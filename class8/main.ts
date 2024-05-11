0bce0c89683eddae2e7ad72ad2a1c2fa44cf62f4
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-05-11 15:47:53 +0500

    v1

--- class8/main.ts
+++ class8/main.ts
@@ -1,15 +1,19 @@
 // Array
 
-let fruits: string[] = ["Mango", "Pome", "Banana"];
+let fruits: string[] = ["Mango", "Pome", "Banana", "Grapes"];
 
-fruits.push("Apple"); // Add apple to the end
+// fruits.push("Apple"); // Add apple to the end
 
-fruits.pop(); // Remove apple to the end
+// fruits.pop(); // Remove apple to the end
 
-fruits.shift(); // Removes the first element "Mango"
+// fruits.shift(); // Removes the first element "Mango"
 
-fruits.unshift("Kiwi"); // Add kiwi to the start 
-
-console.log(fruits);
+// fruits.unshift("Kiwi"); // Add kiwi to the start 
 
 // Slice
+
+let copy = fruits.slice(1,2) // it makes a copy of an array
+
+console.log(fruits);
+console.log(copy);
+
