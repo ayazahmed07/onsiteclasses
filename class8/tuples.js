fb47342d761ed5fad2038069474a339bf5116bed
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-05-11 16:24:32 +0500

    upd

--- class8/tuples.js
+++ class8/tuples.js
@@ -1,1 +1,4 @@
 "use strict";
+// Tuple is typescript feature that doesnt exist in javascript
+let fruitstuple = ["Hamza", 22]; // Set a limit and data type of an array
+console.log(fruitstuple);
--- class8/tuples.ts
+++ class8/tuples.ts
@@ -1,2 +1,5 @@
 // Tuple is typescript feature that doesnt exist in javascript
 
+let fruitstuple: [string, number] = ["Hamza", 22]; // Set a limit and data type of an array
+
+console.log(fruitstuple)
