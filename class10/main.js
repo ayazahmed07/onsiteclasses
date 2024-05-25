fbe47ac6080edaeb5048fda1037cc8495b7a5294
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-05-25 14:58:00 +0500

    class10

--- class10/main.js
+++ class10/main.js
@@ -1,1 +1,13 @@
 "use strict";
+// // Topic "Any" to pass any type of data if never shows error
+// let age: any;
+// age = 333
+// let age1: unknown
+// age1 = "dfdf"
+// let price = 18;
+// console.log(price.toFixed(4));
+let age;
+age = 18;
+console.log(age.toFixed(2)); // Explicit casting consider age as number method 1
+let newAge = age; // method 2
+age; // method 3
---
+++ class10/main.ts
@@ -0,0 +1,32 @@
+// // Topic "Any" to pass any type of data if never shows error
+
+// let age: any;
+
+// age = 333
+
+// let age1: unknown
+
+// age1 = "dfdf"
+
+
+// let price = 18;
+
+// console.log(price.toFixed(4));
+
+
+let age: unknown;
+
+age = 18;
+
+
+console.log((age as number).toFixed(2)) // Explicit casting consider age as number method 1
+
+let newAge = age as number; // method 2
+
+
+(<number>age); // method 3
+
+
+
+
+
