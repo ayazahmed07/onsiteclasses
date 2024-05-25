eb8ac29d0e5f3c72a6f6282291984fe9ccdf70eb
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-05-25 15:39:35 +0500

    class10 function

--- class10/main.js
+++ class10/main.js
@@ -6,8 +6,22 @@
 // age1 = "dfdf"
 // let price = 18;
 // console.log(price.toFixed(4));
-let age;
-age = 18;
-console.log(age.toFixed(2)); // Explicit casting consider age as number method 1
-let newAge = age; // method 2
-age; // method 3
+// let age: unknown;
+// age = 18;
+// console.log((age as number).toFixed(2)) // Explicit casting consider age as number method 1
+// let newAge = age as number; // method 2
+// (<number>age); // method 3
+// function multiplay (a: number, b: number){
+// let multiplication = a * b;
+// return multiplication
+// }
+// console.log(multiplay(5, 10))
+// if you need to get output outside of function then you need to return the function otherwise not
+function sum(...rest) {
+    let total = 0;
+    for (let i = 0; i < rest.length; i++) {
+        total += rest[i];
+    }
+    console.log(total);
+}
+sum(12, 1, 2, 4);
--- class10/main.ts
+++ class10/main.ts
@@ -8,25 +8,38 @@
 
 // age1 = "dfdf"
 
-
 // let price = 18;
 
 // console.log(price.toFixed(4));
 
+// let age: unknown;
 
-let age: unknown;
+// age = 18;
 
-age = 18;
+// console.log((age as number).toFixed(2)) // Explicit casting consider age as number method 1
 
+// let newAge = age as number; // method 2
 
-console.log((age as number).toFixed(2)) // Explicit casting consider age as number method 1
+// (<number>age); // method 3
 
-let newAge = age as number; // method 2
+// function multiplay (a: number, b: number){
+// let multiplication = a * b;
 
+// return multiplication
 
-(<number>age); // method 3
+// }
+// console.log(multiplay(5, 10))
 
+// if you need to get output outside of function then you need to return the function otherwise not
 
+function sum(...rest: number[]): void {
+  let total = 0;
 
+  for (let i = 0; i < rest.length; i++) {
+    total += rest[i];
+  }
 
+  console.log(total)
+}
 
+sum(12, 1, 2, 4);
