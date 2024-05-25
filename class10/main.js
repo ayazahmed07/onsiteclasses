d1d2204bad6790e14c6c87d99ba8da25072d971d
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-05-25 16:36:54 +0500

    overlaod

--- class10/main.js
+++ class10/main.js
@@ -1,27 +1,11 @@
 "use strict";
 // // Topic "Any" to pass any type of data if never shows error
-// let age: any;
-// age = 333
-// let age1: unknown
-// age1 = "dfdf"
-// let price = 18;
-// console.log(price.toFixed(4));
-// let age: unknown;
-// age = 18;
-// console.log((age as number).toFixed(2)) // Explicit casting consider age as number method 1
-// let newAge = age as number; // method 2
-// (<number>age); // method 3
-// function multiplay (a: number, b: number){
-// let multiplication = a * b;
-// return multiplication
-// }
-// console.log(multiplay(5, 10))
-// if you need to get output outside of function then you need to return the function otherwise not
-function sum(...rest) {
-    let total = 0;
-    for (let i = 0; i < rest.length; i++) {
-        total += rest[i];
-    }
-    console.log(total);
+function sum(agr1, arg2) {
+    return agr1 + arg2;
 }
-sum(1, 2, 3, 4);
+console.log(sum(2, 3));
+console.log(sum("2", "3"));
+console.log(sum(2, "3"));
+console.log(sum("2", 3));
+console.log(sum(true, 3));
+console.log(sum(false, 3));
--- class10/main.ts
+++ class10/main.ts
@@ -32,14 +32,36 @@
 
 // if you need to get output outside of function then you need to return the function otherwise not
 
-function sum(...rest: number[]): void {
-  let total = 0;
+// function sum(...rest: number[]): void {
+//   let total = 0;
 
-  for (let i = 0; i < rest.length; i++) {
-    total += rest[i];
-  }
+//   for (let i = 0; i < rest.length; i++) {
+//     total += rest[i];
+//   }
 
-  console.log(total)
+//   console.log(total)
+// }
+
+// sum(1, 2, 3, 4);
+
+// Function Overlaod  - when main function has type any then it will check for over load
+function sum(agr1: number, arg2: number): number; // Overlaod
+
+function sum(agr1: string, arg2: string): string; // Overlaod
+
+function sum(agr1: number, arg2: string): string; // Overlaod
+
+function sum(agr1: string, arg2: number): string; // Overlaod
+
+function sum(agr1: boolean, arg2: number): string; // Overlaod
+
+function sum(agr1: any, arg2: any): any {
+  return agr1 + arg2;
 }
 
-sum(1, 2, 3, 4);
+console.log(sum(2, 3));
+console.log(sum("2", "3"));
+console.log(sum(2, "3"));
+console.log(sum("2", 3));
+console.log(sum(true, 3));
+console.log(sum(false, 3));
