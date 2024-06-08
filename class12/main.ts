03f05a487d126e8f20bd2fa8bfec70f854b41224
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-06-08 16:42:39 +0500

    updated

--- class12/main.ts
+++ class12/main.ts
@@ -51,6 +51,33 @@
 
 //New Topic Promise
 
+type Fn = () => void;
+
+
+function checkmarks (){
+
+return new Promise((resolve, reject) => {
+let marks = 91; 
+setTimeout(() => {
+    if(marks >= 90){
+        resolve("You are passed");
+    }else{
+        reject("You are failed");
+    }
+}, 5000 )
+
+
+})
+
+
+}
+
+checkmarks().then((fff) => {console.log(fff);}).catch((ee) => {
+console.log(ee);
+
+})
+
+
 
 
 
