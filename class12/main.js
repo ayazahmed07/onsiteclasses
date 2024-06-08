74e40364ac861bf902385422f6b918b8805328d9
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-06-08 16:02:58 +0500

    First Commit

--- class12/main.js
+++ class12/main.js
@@ -1,2 +1,35 @@
 "use strict";
-// Async Javascript
+// Async Function Javascript
+//async works pararel where the code takes time to run then we write a seprate code which runs pararel
+// set timeout functions add delay in code and runs in pararlel
+// sysnc runs line by line and waits for code to run before moving to next line
+// async runs line by line and runs in pararlel
+// import inquirer from "inquirer";
+// console.log("First");
+// setTimeout(() => {
+//     console.log("Second");
+// }, 5000)
+// console.log("Third");
+// function pizzaOrder(preparePizzacb: any) {
+//   setTimeout(() => {
+//     console.log("Order Placed");
+//     preparePizzacb(orderdeliver);
+//   }, 6000);
+// }
+// function preparePizza(orderdelivercb: any) {
+//   setTimeout(() => {
+//     console.log("Pizza is prepared");
+//     orderdeliver();
+//   }, 2000);
+// }
+// function orderdeliver() {
+//   setTimeout(() => {
+//     console.log("Order is Delivered");
+//   }, 10000);
+// }
+// pizzaOrder(preparePizza)
+// TO MAKE FUNCTION TYPE
+// type Fn = () => void;
+// function Pizza(prepare: Fn){
+// }
+//New Topic Promise
