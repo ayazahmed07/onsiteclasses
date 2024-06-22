b6855ed4ba53d73aa92d8cf6d0109cd9e21c441f
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-06-22 14:49:38 +0500

    class

--- class13 Object Oriented Programming/main.js
+++ class13 Object Oriented Programming/main.js
@@ -1,5 +1,16 @@
 "use strict";
 // Object Oriented Programming / by doing object oriented programming we can organize our code
 // Object Base programming
-// Class: A blueprint for creating objects. It defines a datatype by bundling data (attributes) and methods (functions or procedures).
+// Class: A blueprint for creating objects. It defines a datatype by bundling data (attributes) and methods (functions or procedures). always start with capital letter
 // Object: An instance of a class. When a class is defined, no memory is allocated until an object of that class is created.
+// we always write new before class when using calss
+class House {
+    constructor() {
+        this.ownerName = "Ayaz";
+        this.address = "House no F 818 Al fatah colony orangi";
+        this.numberofDoors = 2;
+    }
+}
+let h1 = new House();
+console.log(h1.address);
+console.log(h1["address"]); // to acces addres
--- class13 Object Oriented Programming/main.ts
+++ class13 Object Oriented Programming/main.ts
@@ -2,6 +2,17 @@
 
 // Object Base programming
 
-// Class: A blueprint for creating objects. It defines a datatype by bundling data (attributes) and methods (functions or procedures).
+// Class: A blueprint for creating objects. It defines a datatype by bundling data (attributes) and methods (functions or procedures). always start with capital letter
 // Object: An instance of a class. When a class is defined, no memory is allocated until an object of that class is created.
+// we always write new before class when using calss
 
+class House {
+  ownerName: string = "Ayaz";
+  address: string = "House no F 818 Al fatah colony orangi";
+  numberofDoors: number = 2;
+}
+
+let h1 = new House();
+
+console.log(h1.address)
+console.log(h1["address"]) // to acces addres
