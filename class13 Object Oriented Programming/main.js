01f6090411b1addfd050b4989d1bc32fa048ba47
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-06-23 12:36:52 +0500

    ums

--- class13 Object Oriented Programming/main.js
+++ class13 Object Oriented Programming/main.js
@@ -4,13 +4,42 @@
 // Class: A blueprint for creating objects. It defines a datatype by bundling data (attributes) and methods (functions or procedures). always start with capital letter
 // Object: An instance of a class. When a class is defined, no memory is allocated until an object of that class is created.
 // we always write new before class when using calss
-class House {
-    constructor() {
-        this.ownerName = "Ayaz";
-        this.address = "House no F 818 Al fatah colony orangi";
-        this.numberofDoors = 2;
+// class House {
+//   ownerName: string;
+//   readonly address: string;
+//   numberofDoors: number;
+//   constructor(name: string, Address: string, numberofDoors: number) {
+//     (this.ownerName = name),
+//       (this.address = Address),
+//       (this.numberofDoors = numberofDoors);
+//   } // to access class properties we use this. in constructor  it is a function which calls object
+//   bell() {
+//     console.log(this.address, "Ding Dong");
+//   } // Methods
+// }
+// let h1 = new House("Ayaz", "House 818", 5);
+// let h2 = new House("Faris", "House 320", 10);
+// h1.bell()
+// h2.bell()
+// Newtopic Inheritance and Encapsulation
+class Animal {
+    constructor(aniname, aniage) {
+        (this.name = aniname), (this.age = aniage);
+    }
+    eat() {
+        console.log("Eating");
     }
 }
-let h1 = new House();
-console.log(h1.address);
-console.log(h1["address"]); // to acces addres
+class Cat extends Animal {
+    constructor(name, age, eyecolor) {
+        super(name, age);
+        this.color = eyecolor;
+    }
+    meow() {
+        console.log("Meao Meao");
+    }
+}
+let cat1 = new Cat("Kochi", 4, "blue");
+console.log(cat1);
+cat1.meow();
+cat1.eat();
--- class13 Object Oriented Programming/main.ts
+++ class13 Object Oriented Programming/main.ts
@@ -6,13 +6,58 @@
 // Object: An instance of a class. When a class is defined, no memory is allocated until an object of that class is created.
 // we always write new before class when using calss
 
-class House {
-  ownerName: string = "Ayaz";
-  address: string = "House no F 818 Al fatah colony orangi";
-  numberofDoors: number = 2;
+// class House {
+//   ownerName: string;
+//   readonly address: string;
+//   numberofDoors: number;
+
+//   constructor(name: string, Address: string, numberofDoors: number) {
+//     (this.ownerName = name),
+//       (this.address = Address),
+//       (this.numberofDoors = numberofDoors);
+//   } // to access class properties we use this. in constructor  it is a function which calls object
+
+//   bell() {
+//     console.log(this.address, "Ding Dong");
+//   } // Methods
+// }
+
+// let h1 = new House("Ayaz", "House 818", 5);
+// let h2 = new House("Faris", "House 320", 10);
+
+// h1.bell()
+// h2.bell()
+
+// Newtopic Inheritance and Encapsulation
+
+class Animal {
+  name: string;
+  age: number;
+
+  constructor(aniname: string, aniage: number) {
+    (this.name = aniname), (this.age = aniage);
+  }
+
+  eat() {
+    console.log("Eating");
+  }
 }
 
-let h1 = new House();
+class Cat extends Animal {
+  color: string;
 
-console.log(h1.address)
-console.log(h1["address"]) // to acces addres
+  constructor(name: string, age: number, eyecolor: string) {
+    super(name, age);
+    this.color = eyecolor;
+  }
+
+  meow() {
+    console.log("Meao Meao");
+  }
+}
+
+let cat1 = new Cat("Kochi", 4, "blue");
+
+console.log(cat1);
+cat1.meow();
+cat1.eat();
